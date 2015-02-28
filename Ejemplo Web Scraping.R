aede51e6e30959995cc9bc54d6184be0cb8e75a3
luismart <l.martinezuribe@gmail.com>
2015-02-28 17:17:43 +0100

    Signed-off-by: luismart <l.martinezuribe@gmail.com>

--- Ejemplo Web Scraping.R
+++ Ejemplo Web Scraping.R
@@ -22,3 +22,34 @@ eleccion <- as.data.frame(eleccion)
 # lo exportamos a un fichero csv
 write.table(eleccion, "eleccion.csv", sep=";")
 
+
+# Si queremos obtener los datos para varios municipios utilizamos un For
+eleccion_full <- data.frame(matrix(ncol = 4))
+colnames(eleccion_full) <- c("Municipio","Partido" ,"Concejales" , "Votos")
+
+for (i in 11:40){
+  
+  # URL of interest: 
+  mps <- paste("http://resultados.elpais.com/elecciones/2011/municipales/12/28/",i,".html",sep="")
+  
+  # primero convertimos el documento para poder navegarlo
+  mps.doc <- htmlParse(mps, encoding="UTF-8")
+  
+  # miramos el html para ver las etiquetas de donde se sacan los valores que necesitamos
+  Municipio <- xpathSApply(mps.doc,"//div[@class='resultado-unidad estirar']/h1",xmlValue)
+  Partido <- xpathSApply(mps.doc,"//th[@class='nombrePartido']",xmlValue)
+  Concejales <- xpathSApply(mps.doc,"//td[@class='tipoNumeroElectos']",xmlValue)
+  Votos <- xpathSApply(mps.doc,"//td[@class='tipoNumeroVotos']",xmlValue)
+  
+  # lo convertimos en una tabla
+  Municipio <- gsub("\nver resultados en autonómicas »\n","",Municipio)
+  Municipio <- rep(Municipio,4)
+  eleccion <- cbind (Municipio,Partido,Concejales,Votos)
+  eleccion <- as.data.frame(eleccion)
+  
+  eleccion_full <- rbind (eleccion_full,eleccion)
+  writeLines(paste("iteración: ",i))
+}
+
+# lo exportamos a un fichero csv
+write.table(eleccion, "eleccion.csv", sep=";")
